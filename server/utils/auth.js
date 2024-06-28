4ffdbd9a9c38979190186dd3389874bc4ca7fe60
bkness <kbrandon863@gmail.com>
2024-06-28 15:34:17 -0700

    reverted Auth.js back to original jsonwebtoken insteadt of apollo-server-express

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,33 +1,37 @@
-const { AuthenticationError } = require('apollo-server-express');
+// const { AuthenticationError } = require('apollo-server-express'); // import AuthenticationError from apollo-server-express
 const jwt = require('jsonwebtoken');
+const {GraphQLError} = require('graphql');
 
-const secret = 'secretKEY'; // Update with a secure secret key
+const secret = 'secretKEY'; // update with secret key
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('cannot authenticate user', {
+    extensions: {
+      code: 'UNAUTHENTICATED'
+    }
+  }), // export AuthenticationError directly
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
-      // Remove 'Bearer ' from token string
+      // remove 'Bearer ' from token string
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return req; // Return the original req object if no token found
+      return req;
     }
 
     try {
-      const { data } = jwt.verify(token, secret);
-      req.user = data; // Attach user data to req object if token is valid
+      const { data } = jwt.verify(token, secret, {maxAge: expiration});
+      req.user = data;
     } catch (error) {
       console.error('Invalid token:', error.message);
-      throw new AuthenticationError('Invalid token');
     }
 
-    return req; // Return req object with or without user data
+    return req;
   },
-
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
