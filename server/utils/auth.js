4c6dff4c860f5679d6feeb351b7147ab9ef6a397
Leo Wong <leoelicos@gmail.com>
2024-07-07 22:57:14 +1000

    Revert comment out AuthenticationError

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
-const {GraphQLError} = require('graphql');
+const { GraphQLError } = require('graphql');
 
 const secret = 'secretKEY'; // update with secret key
 const expiration = '2h';
 
 module.exports = {
-  // AuthenticationError: new GraphQLError('cannot authenticate user', {
-  //   extensions: {
-  //     code: 'UNAUTHENTICATED'
-  //   }
-  // }), // export AuthenticationError directly
+  AuthenticationError: new GraphQLError('cannot authenticate user', {
+    extensions: {
+      code: 'UNAUTHENTICATED'
+    }
+  }), // export AuthenticationError directly
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -23,7 +23,7 @@ module.exports = {
     }
 
     try {
-      const { data } = jwt.verify(token, secret, {maxAge: expiration});
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch (error) {
       console.error('Invalid token:', error.message);
@@ -34,5 +34,5 @@ module.exports = {
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
 };
