3442f1c761dd99c2c7babbbc515fde7b17e0d979
Brandon Kelly <kbrandon863@gmail.com>
2024-07-16 15:15:10 -0700

    Refactored NewsPage and NewsItem components to handle missing data

---
+++ server/schemas/mockNews.js
@@ -0,0 +1,286 @@
+module.exports = {
+	status: 200,
+	data: {
+		articles: [
+			{
+				source: 'The Washington Post',
+				author: 'Ben Golliver',
+				title:
+					'Thanks, Obama: USA Basketball survives rust, roster drama in exhibition win - The Washington Post',
+				description:
+					'With the former president sitting courtside, a star-studded U.S. team beat Canada in its first tune-up before the Paris Olympics.',
+				url: 'https://www.washingtonpost.com/sports/olympics/2024/07/11/usa-basketball-canada-barack-obama/',
+				urlToImage:
+					'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/URHPROSA2BCB5A3XBGLQBACS2U_size-normalized.jpg&w=1440',
+				publishedAt: '2024-07-11T14:21:34Z',
+				content:
+					'LAS VEGAS Heavy favorites make for big targets.\r\nThe United States mens basketball team found itself entangled in a chaotic mess as it took the court to launch its run to the Paris Olympics: Two star… [+6837 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'New York Post',
+				author: 'Tracy Swartz',
+				title:
+					'40% of cancer cases linked to bad behavior — here are the major culprits - New York Post ',
+				description: 'Well, this is a drag.',
+				url: 'https://nypost.com/2024/07/11/lifestyle/40-of-cancer-cases-linked-to-bad-behavior-smoking-main-culprit/',
+				urlToImage:
+					'https://nypost.com/wp-content/uploads/sites/2/2024/07/portrait-middle-aged-woman-smoking-85275289.jpg?quality=75&strip=all&w=1024',
+				publishedAt: '2024-07-11T14:05:00Z',
+				content:
+					'Well, this is a drag.\r\nSmoking is to blame for 20% of US cancer cases and nearly 30% of cancer deaths, the American Cancer Society (ACS) warns in new research.\r\nIn fact, the organization insists that… [+2372 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'Google News',
+				author: 'ABC News',
+				title:
+					'Gunman at large after ambushing, killing deputy following assault on pizza worker over wrong order: Officials - ABC News',
+				description: '',
+				url: 'https://news.google.com/rss/articles/CBMiZmh0dHBzOi8vYWJjbmV3cy5nby5jb20vVVMvZ3VubWFuLWxhcmdlLWFmdGVyLWFtYnVzaGluZy1raWxsaW5nLWRlcHV0eS1hc3NhdWx0LXBpenphL3N0b3J5P2lkPTExMTg0MDgyONIBamh0dHBzOi8vYWJjbmV3cy5nby5jb20vYW1wL1VTL2d1bm1hbi1sYXJnZS1hZnRlci1hbWJ1c2hpbmcta2lsbGluZy1kZXB1dHktYXNzYXVsdC1waXp6YS9zdG9yeT9pZD0xMTE4NDA4Mjg?oc=5',
+				urlToImage: '',
+				publishedAt: '2024-07-11T13:23:53Z',
+				content: '',
+				__typename: 'News',
+			},
+			{
+				source: 'Reuters',
+				author: 'Reuters',
+				title: 'Fed seen nearing rate cuts as inflation eases - Reuters',
+				description: '',
+				url: 'https://www.reuters.com/markets/us/traders-boost-bets-fed-rate-cut-sept-after-cpi-data-2024-07-11/',
+				urlToImage: '',
+				publishedAt: '2024-07-11T13:18:00Z',
+				content: '',
+				__typename: 'News',
+			},
+			{
+				source: 'ABC News',
+				author: 'Darren Reynolds',
+				title:
+					'Brawl breaks out between players and fans after Colombia-Uruguay soccer match - ABC News',
+				description: 'About a dozen Uruguay players went into the stands.',
+				url: 'https://abcnews.go.com/International/brawl-breaks-players-fans-after-colombia-uruguay-copa/story?id=111834692',
+				urlToImage:
+					'https://i.abcnewsfe.com/a/5c1669f0-f392-4078-9c22-a81b2bf8e943/uruguay-colombia-jul_1720670746724_hpMain_16x9.jpg?w=1600',
+				publishedAt: '2024-07-11T13:07:38Z',
+				content:
+					'After a heated soccer match on the pitch, emotions spilled over into the stands Wednesday night at the Colombia/Uruguay Copa America semi-final soccer match in Charlotte.\r\nAbout a dozen Uruguay socce… [+269 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'CNET',
+				author: '',
+				title:
+					"Galaxy Z Flip 6 vs Z Flip 5 vs. Z Flip 4: Comparing Samsung's Flip Phones - CNET",
+				description:
+					"All the specs and prices for Samsung's latest Galaxy Z Flip 6 phone compared with the Galaxy Z Flip 5 and the Galaxy Z Flip 4.",
+				url: 'https://www.cnet.com/tech/mobile/galaxy-z-flip-6-z-flip-5-z-flip-4-comparing-samsung-flip-phones/',
+				urlToImage:
+					'https://www.cnet.com/a/img/resize/39f7c79965ff3778667f14b9c182b8445d2fd356/hub/2024/07/10/a58b5d7e-0720-42cc-a5d8-a06c391af1d8/galaxyzflip6.png?auto=webp&fit=crop&height=675&width=1200',
+				publishedAt: '2024-07-11T13:00:00Z',
+				content:
+					'On Wednesday Samsung unveiled the Galaxy Z Flip 6, its next-gen clamshell-style flip phone. The phone has a higher-res camera, a better durability rating, more RAM and a suite of fresh AI features de… [+5402 chars]',
+				__typename: 'News',
+			},
+			{
+				source: '[Removed]',
+				author: '',
+				title: '[Removed]',
+				description: '[Removed]',
+				url: 'https://removed.com',
+				urlToImage: '',
+				publishedAt: '1970-01-01T00:00:00Z',
+				content: '[Removed]',
+				__typename: 'News',
+			},
+			{
+				source: 'The Wall Street Journal',
+				author: 'The Wall Street Journal',
+				title:
+					'Delta’s Sagging Profit Signals Trouble for Airlines This Summer - The Wall Street Journal',
+				description: '',
+				url: 'https://www.wsj.com/business/airlines/delta-dal-q2-earnings-report-2024-e025fb71',
+				urlToImage: '',
+				publishedAt: '2024-07-11T12:34:00Z',
+				content: '',
+				__typename: 'News',
+			},
+			{
+				source: 'CNN',
+				author: 'Elizabeth Wolfe, Ashley Killough, Ed Lavandera',
+				title:
+					'Anger mounts in southeast Texas as crippling power outages and heat turn deadly - CNN',
+				description:
+					'Frustrations are mounting across southeast Texas as residents enter a fourth day of crippling power outages and heat, a combination that has proven dangerous – and at times deadly – as residents struggle to access food, gas and medical care.',
+				url: 'https://www.cnn.com/2024/07/11/weather/texas-heat-beryl-power-outage-thursday/index.html',
+				urlToImage:
+					'https://media.cnn.com/api/v1/images/stellar/prod/2024-07-10t063056z-1116356832-rc22s8ad9tn0-rtrmadp-3-usa-weather-20240711035602342.jpg?c=16x9&q=w_800,c_fill',
+				publishedAt: '2024-07-11T12:09:00Z',
+				content:
+					'Frustrations are mounting across southeast Texas as residents enter a fourth day of crippling power outages and heat, a combination that has proven dangerous and at times deadly as some struggle to a… [+6793 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'CBS Sports',
+				author: '',
+				title:
+					"2024 NFL head coach rankings: Chiefs' Andy Reid still king as Chargers' Jim Harbaugh returns with top 10 spot - CBS Sports",
+				description: 'A rundown of every single NFL coach, 32-1',
+				url: 'https://www.cbssports.com/nfl/news/2024-nfl-head-coach-rankings-chiefs-andy-reid-still-king-as-chargers-jim-harbaugh-returns-with-top-10-spot/',
+				urlToImage:
+					'https://sportshub.cbsistatic.com/i/r/2024/07/10/0ddd970e-efa6-4912-bd23-48a8b5f113ad/thumbnail/1200x675/6d63a73716518f8dda389407fed1b087/andy-reid-g.jpg',
+				publishedAt: '2024-07-11T12:04:05Z',
+				content:
+					"Football is a team sport. Which means, more often than not, you won't be a serious Super Bowl contender if you don't have a formidable combination of elite talent and/or consistent, innovative coachi… [+18990 chars]",
+				__typename: 'News',
+			},
+			{
+				source: 'ESPN',
+				author: 'Jeff Carlisle',
+				title:
+					'Why U.S. Soccer had no choice but to fire USMNT coach Berhalter - ESPN',
+				description:
+					"Gregg Berhalter is out. If U.S. Soccer sporting director Matt Crocker gets the next men's national team coach wrong, it might be Crocker who's out then.",
+				url: 'https://www.espn.com/soccer/story/_/id/40536813/us-soccer-cant-miss-next-usmnt-coach-gregg-berhalter-analysis',
+				urlToImage:
+					'https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2024%2F0711%2Fr1357190_1296x729_16%2D9.jpg',
+				publishedAt: '2024-07-11T12:00:00Z',
+				content:
+					"During Gregg Berhalter's tenure as manager of the U.S. men's national soccer team, he always seemed to do just enough to meet modest expectations. But he also never exceeded them in a way that could … [+6892 chars]",
+				__typename: 'News',
+			},
+			{
+				source: 'CNN',
+				author: 'Catherine Nicholls, Sana Noor Haq',
+				title:
+					'Tributes flood in for BBC sport commentator whose wife and daughters were killed in suspected crossbow attack - CNN',
+				description:
+					'Figures from across the sporting world have offered their condolences to a BBC sport commentator, after his wife and two daughters were killed by an alleged crossbow attacker, in deaths that again drew attention to the epidemic of violence against women.',
+				url: 'https://www.cnn.com/2024/07/11/europe/crossbow-attack-violence-women-bbc-journalist-intl-gbr/index.html',
+				urlToImage:
+					'https://media.cnn.com/api/v1/images/stellar/prod/source-bbc.JPG?c=16x9&q=w_800,c_fill',
+				publishedAt: '2024-07-11T11:53:00Z',
+				content:
+					'Figures from across the sporting world have offered their condolences to a BBC sport commentator, after his wife and two daughters were killed by an alleged crossbow attacker, in deaths that again dr… [+3315 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'Space.com',
+				author: 'Robert Lea',
+				title:
+					'Hubble Space Telescope finds closest massive black hole to Earth — a cosmic clue frozen in time - Space.com',
+				description:
+					"It's found in Omega Centauri, the remains of a cannibalized galaxy.",
+				url: 'https://www.space.com/closest-massive-black-hole-earth-hubble',
+				urlToImage:
+					'https://cdn.mos.cms.futurecdn.net/Pzd9Mx7tpAGNEXBnNL6fDV-1200-80.png',
+				publishedAt: '2024-07-11T11:48:24Z',
+				content:
+					'Using the Hubble Space Telescope, astronomers have discovered the closest massive black hole to Earth ever seen, a cosmic titan "frozen in time." \r\nAs an example of an elusive "intermediate-mass blac… [+7280 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'BBC News',
+				author: '',
+				title: 'US cruise missiles to return to Germany, angering Moscow - BBC.com',
+				description:
+					'The missiles will be deployed periodically from 2026 for the first time since the Cold War.',
+				url: 'https://www.bbc.com/news/articles/cgxq7lkj4vgo',
+				urlToImage:
+					'https://ichef.bbci.co.uk/news/1024/branded_news/9287/live/e7c54250-3f76-11ef-aab4-abea2b917c4b.jpg',
+				publishedAt: '2024-07-11T11:35:42Z',
+				content:
+					"Long-range US missiles are to be deployed periodically in Germany from 2026 for the first time since the Cold War, in a decision announced at Nato's 75th anniversary summit.\r\nThe Tomahawk cruise, SM-… [+2399 chars]",
+				__typename: 'News',
+			},
+			{
+				source: 'Associated Press',
+				author: 'KEN MORITSUGU',
+				title:
+					"China tells NATO not to create chaos in Asia and rejects label of 'enabler' of Russia's Ukraine war - The Associated Press",
+				description:
+					'China has accused NATO of seeking security at the expense of others and told the alliance not to bring the same “chaos” to Asia. The statement from a Foreign Ministry spokesperson on Thursday came a day after NATO labeled China a “decisive enabler” of Russia’…',
+				url: 'https://apnews.com/article/china-nato-ukraine-war-689834988a7e0543504010649a63e319',
+				urlToImage:
+					'https://dims.apnews.com/dims4/default/2013eb5/2147483647/strip/true/crop/3293x1852+0+172/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F91%2Fa8%2F229f9fd1f23b0e7233e74ec17420%2F287478d1170e4e1e9504347185db67bb',
+				publishedAt: '2024-07-11T11:27:00Z',
+				content:
+					'BEIJING (AP) China accused NATO on Thursday of seeking security at the expense of others and told the alliance not to bring the same chaos to Asia, a reflection of its determination to oppose strengt… [+3278 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'NPR',
+				author: '',
+				title:
+					'Biden is holding a rare solo news conference to try to show he’s up for this campaign - NPR',
+				description:
+					'President Biden’s first formal solo news conference of the year is being closely watched to see how he handles curveballs. Many in his own party want him to step back from his reelection bid.',
+				url: 'https://www.npr.org/2024/07/11/nx-s1-5033845/joe-biden-nato-press-conference',
+				urlToImage:
+					'https://npr.brightspotcdn.com/dims3/default/strip/false/crop/5105x2872+0+266/resize/1400/quality/100/format/jpeg/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2F42%2Fb8%2F2ea743e34194b2c0f15ebacaaa90%2Fgettyimages-2160760781.jpg',
+				publishedAt: '2024-07-11T11:00:00Z',
+				content:
+					'President Biden faces yet another high-profile public test of whether hes sharp enough to campaign for a second term when he takes questions from reporters on Thursday.\r\nIt comes as Biden wraps up a … [+2631 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'New York Post',
+				author: 'Steven Vago, Priscilla DeGregory',
+				title:
+					'New Mexicans aren’t happy to see ‘narcissistic’ Alec Baldwin in Santa Fe for involuntary manslaughter trial - New York Post ',
+				description:
+					'“It’s egotistical, it’s narcissistic. We’ve noticed that when he played Trump. He played that part easy,” one local woman who attended the trial fumed.',
+				url: 'https://nypost.com/2024/07/11/us-news/new-mexicans-slam-alec-baldwin-as-manslaughter-trial-kicks-off/',
+				urlToImage:
+					'https://nypost.com/wp-content/uploads/sites/2/2024/07/actor-alec-baldwin-arrives-district-85235014.jpg?quality=75&strip=all&w=1024',
+				publishedAt: '2024-07-11T10:43:00Z',
+				content:
+					'SANTA FE, N.M. — Santa Fe is used to tourists. But not like this. \r\nAlec Baldwin swept into town this week for the start of his involuntary manslaughter trial, and he’s not exactly getting a warm wel… [+5373 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'CBS News',
+				author: '',
+				title:
+					'Woman swimming off Japanese beach was swept into the Pacific, but rescued 37 hours later and 50 miles away - CBS News',
+				description:
+					'A cargo ship crew spotted a Chinese woman who had drifted 50 miles from the Japanese beach where she went for a swim.',
+				url: 'https://www.cbsnews.com/news/woman-rescued-pacific-swimming-japan-beach-adrift-37-hours/',
+				urlToImage:
+					'https://assets2.cbsnewsstatic.com/hub/i/r/2024/07/11/a176c7ae-4283-4a28-a693-495ea978b35b/thumbnail/1200x630/b2e3c5a73320da30cf70d5684630bd7f/ap24193209814094.jpg?v=2287029998c5246c93d6dd038eb30603',
+				publishedAt: '2024-07-11T10:31:00Z',
+				content:
+					'Tokyo — A Chinese woman who was swept out to sea while swimming at a Japanese beach was rescued 37 hours later after drifting in a swimming ring more than 50 miles in the Pacific Ocean, officials sai… [+1140 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'NBC News',
+				author: 'Patrick Smith',
+				title:
+					"Chinese military ships spotted near Alaska's Aleutian islands - NBC News",
+				description:
+					'Chinese Navy ships were spotted inside the U.S. exclusive economic zone of the Bering Sea by a cutter on a routine patrol late last week, the U.S.',
+				url: 'https://www.nbcnews.com/news/us-news/china-military-ships-alaska-us-coast-guard-rcna161304',
+				urlToImage:
+					'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2024-07/240711-bering-sea-uscg-mb-1049-bdb854.jpg',
+				publishedAt: '2024-07-11T10:08:35Z',
+				content:
+					'Chinese Navy ships were spotted inside the U.S. exclusive economic zone of the Bering Sea by a cutter on a routine patrol late last week, the U.S. Coast Guard said on Wednesday.\r\nThe U.S. Coast Guard… [+1548 chars]',
+				__typename: 'News',
+			},
+			{
+				source: 'Google News',
+				author: '9to5Mac',
+				title:
+					'iPhone 16 Pro to get same 5x zoom camera as Pro Max, bigger upgrades next year - 9to5Mac',
+				description: '',
+				url: 'https://news.google.com/rss/articles/CBMiRWh0dHBzOi8vOXRvNW1hYy5jb20vMjAyNC8wNy8xMS9pcGhvbmUtMTYtcHJvLW9wdGljYWwtem9vbS10ZXRyYXByaXNtL9IBAA?oc=5',
+				urlToImage: '',
+				publishedAt: '2024-07-11T10:04:00Z',
+				content: '',
+				__typename: 'News',
+			},
+		],
+	},
+}
