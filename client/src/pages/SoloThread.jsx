f5cb88b5f08b53ffab5074b5145b6f060272bf12
Brandon Kelly <kbrandon863@gmail.com>
2024-07-15 18:50:46 -0700

    Added back to forums button on solo threads

--- client/src/pages/SoloThread.jsx
+++ client/src/pages/SoloThread.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { GET_POST_BY_ID } from '../utils/queries'
 import Button from '@mui/material/Button'
 import Comments from './Comments'
 import CommentForm from './CommentForm'
 
-// grabs single post by id when user clicks view threads, then the user can make a comment on that post
 const SinglePostPage = () => {
 	const { postId } = useParams()
 	const { loading, error, data } = useQuery(GET_POST_BY_ID, {
 		variables: { postId },
 	})
 
-	const [showCommentForm, setShowCommentForm] = useState(false) // State to control showing/hiding the comment form
+	const [showCommentForm, setShowCommentForm] = useState(false)
 
 	if (loading) {
 		return <p>Loading...</p>
@@ -33,29 +32,47 @@ const SinglePostPage = () => {
 	}
 
 	return (
-		<div className="content-container">
-			<div className="forum-card-container">
-				<div className="forum-card">
-					<div className="post">
-						<h2 className="post-title">{post.title}</h2>
-						<p className="author">Posted by: {post.author.username}</p>
-						<p className="created-at">
-							{' '}
-							{new Date(parseInt(post.createdAt)).toLocaleDateString()}
-						</p>
-						<p className="post-content">{post.content}</p>
-						<Button onClick={handleCommentClick} variant="contained" color="primary">
-							Comment
-						</Button>
-						{showCommentForm && (
-							<CommentForm postId={postId} onCommentAdded={handleCommentAdded} />
-						)}{' '}
-						{/* Render the comment form only if showCommentForm is true */}
-						<Comments postId={postId} />
+		<>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '25vh',
+				}}
+			>
+				<Button
+					component={Link}
+					to="/blog"
+					variant="contained"
+					color="primary"
+					style={{ textDecoration: 'none', color: 'inherit' }}
+				>
+					Back to Forums
+				</Button>
+			</div>
+			<div className="content-container">
+				<div className="forum-card-container">
+					<div className="forum-card">
+						<div className="post">
+							<h2 className="post-title">{post.title}</h2>
+							<p className="author">Posted by: {post.author.username}</p>
+							<p className="created-at">
+								{new Date(parseInt(post.createdAt)).toLocaleDateString()}
+							</p>
+							<p className="post-content">{post.content}</p>
+							<Button onClick={handleCommentClick} variant="contained" color="primary">
+								Comment
+							</Button>
+							{showCommentForm && (
+								<CommentForm postId={postId} onCommentAdded={handleCommentAdded} />
+							)}
+							<Comments postId={postId} />
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
